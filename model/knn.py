68b69e537a9ea3cb18581af1beb7148ee8f292fe
derenlei <derenlei@gmail.com>
2025-01-05 22:25:37 -0800

    Update knn.py

--- model/knn.py
+++ model/knn.py
@@ -1,3 +1,26 @@
+import torch
+import torch.nn.functional as F
+
+class KNN:
+    def __init__(self, features, labels, k):
+        self.k = k
+        self.features = features
+        self.labels = labels
+
+    def predict(self, queries):
+        # queries (num_queries, n_features)
+        # torch.cdist(query, data, p=2)
+        # same as 
+        # query.unsqueeze(1)  # Shape: (n_queries, 1, n_features)
+        # data.unsqueeze(0)    # Shape: (1, n_data, n_features)
+        # (n_queries, n_data)
+        dist = torch.sqrt(((query.squeeze(1) - self.features(0))**2).sum(-1))
+        topks = torch.argmin(dist, dim=-1)[:self.k]
+        knn_labels = labels[topks] # (n_queries, labels)
+        predicted_labels = torch.mode(knn_labels, dim=-1).values
+        return predicted_labels
+
+# -----------------------------numpy-version-----------------------
 import numpy as np
 
 from dataclasses import dataclass
