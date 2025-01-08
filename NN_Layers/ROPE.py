49782ac44beeafc880012c838de742a5af0b131e
derenlei <derenlei@gmail.com>
2025-01-08 15:51:51 -0800

    Update ROPE.py

--- NN_Layers/ROPE.py
+++ NN_Layers/ROPE.py
@@ -1,7 +1,12 @@
 # used once when init transformer model
 def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
+    # tensor of frequencies, decreasing exponentially with respect to the positional encoding dimension.
     freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
+
+    # A range of values from 0 to end - 1, representing discrete time steps.
     t = torch.arange(end, device=freqs.device, dtype=torch.float32)
+
+    # 
     freqs = torch.outer(t, freqs)
     freqs_cis = torch.polar(torch.ones_like(freqs), freqs)  # complex64
     return freqs_cis
@@ -25,8 +30,11 @@ def apply_rotary_emb(
     xk: torch.Tensor,
     freqs_cis: torch.Tensor,
 ) -> Tuple[torch.Tensor, torch.Tensor]:
+    
+    # *xq.shape[:-1] = shape1, shape2, ... to keep prev shape same in reshaping
     xq_ = torch.view_as_complex(xq.float().reshape(*xq.shape[:-1], -1, 2))
     xk_ = torch.view_as_complex(xk.float().reshape(*xk.shape[:-1], -1, 2))
+    
     # [seq_len,1,head_dim] -> [1,seq_len,1,head_dim]
     freqs_cis = reshape_for_broadcast(freqs_cis, xq_)
 
