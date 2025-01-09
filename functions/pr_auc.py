9f142b96b49e0fc7c31b032c6bc0be1bf21348a6
derenlei <derenlei@gmail.com>
2025-01-08 21:48:56 -0800

    Update pr_auc.py

--- functions/pr_auc.py
+++ functions/pr_auc.py
@@ -1,3 +1,11 @@
+import numpy as np
+def precision(y_true, y_pred):
+	# Your code here
+	tp = np.sum(y_true==1 & y_pred == 1)
+	fp = np.sum(y_true==0 & y_pred == 1)
+	precision = tp/(tp+fp)
+	return precision
+	
 import torch
 
 def compute_pr_auc_no_loop_no_trapz(y_true, y_pred):
